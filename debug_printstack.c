484fdd34a8ce11d543edad11e4e3c9cc68f3b20d
Vineet Meharia <vmeharia@c4a11c4.42berlin.de>
2026-06-07 00:50:59 +0200

    norm till buildstack

--- debug_printstack.c
+++ debug_printstack.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug_printstack.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmeharia <vmeharia@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/07 00:50:00 by vmeharia          #+#    #+#             */
+/*   Updated: 2026/06/07 00:50:20 by vmeharia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include <stdio.h>
 
 void	debug_printstack(int argc, char **argv)
 {
-	t_node *ptr;
+	t_node	*ptr;
 
 	ptr = build_stack(argc, argv);
 	while (ptr != NULL)
